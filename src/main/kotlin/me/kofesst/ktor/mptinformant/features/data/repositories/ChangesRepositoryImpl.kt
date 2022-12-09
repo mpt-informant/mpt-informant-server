f05b395b71aefdfc8a73cab07ccae47f32e36d2c
kofesst <xkof3st.mail@gmail.com>
2022-12-09 18:51:16 +0300

    Парсинг строки замен вынесен в отдельную функцию в отдельный файл

--- src/main/kotlin/me/kofesst/ktor/mptinformant/features/data/repositories/ChangesRepositoryImpl.kt
+++ src/main/kotlin/me/kofesst/ktor/mptinformant/features/data/repositories/ChangesRepositoryImpl.kt
@@ -1,26 +1,18 @@
 package me.kofesst.ktor.mptinformant.features.data.repositories
 
-import me.kofesst.ktor.mptinformant.features.data.utils.calendar
-import me.kofesst.ktor.mptinformant.features.data.utils.getDayOfWeek
-import me.kofesst.ktor.mptinformant.features.data.utils.parseDocument
-import me.kofesst.ktor.mptinformant.features.data.utils.toDate
+import me.kofesst.ktor.mptinformant.features.data.utils.*
 import me.kofesst.ktor.mptinformant.features.domain.models.changes.GroupChanges
 import me.kofesst.ktor.mptinformant.features.domain.models.changes.GroupChangesDay
 import me.kofesst.ktor.mptinformant.features.domain.models.changes.GroupChangesRow
 import me.kofesst.ktor.mptinformant.features.domain.repositories.ChangesRepository
 import me.kofesst.ktor.mptinformant.features.domain.repositories.GroupsRepository
 import org.jsoup.nodes.Element
-import java.text.SimpleDateFormat
-import java.util.*
 
 class ChangesRepositoryImpl(
     private val groupsRepository: GroupsRepository,
 ) : ChangesRepository {
     companion object {
         private const val ROOT_URL = "https://mpt.ru/studentu/izmeneniya-v-raspisanii/"
-        private val rowTimestampFormatter = SimpleDateFormat(
-            "dd.MM.yyyy HH:mm:ss", Locale.ROOT
-        )
     }
 
     override suspend fun getChangesByGroupId(groupId: String): GroupChanges? = parseDocument(
@@ -66,17 +58,12 @@ class ChangesRepositoryImpl(
     }
 
     private fun parseChangesRow(tableRowElement: Element): GroupChangesRow {
-        val cols = tableRowElement.children()
-        val number = cols[0].text().toInt()
-        val oldSubject = cols[1].text()
-        val newSubject = cols[2].text()
-        val timestamp = (rowTimestampFormatter.parse(cols[3].text()) ?: Date(0)).time
-
-        return if (oldSubject.lowercase() == "дополнительное занятие") {
-            GroupChangesRow.Additional(number, newSubject, timestamp)
-        } else {
-            GroupChangesRow.Replace(number, oldSubject, newSubject, timestamp)
-        }
+        val columns = tableRowElement.children()
+        val rowNumber = columns[0].text().toInt()
+        val replacedString = columns[1].text()
+        val replacementString = columns[2].text()
+        val insertTimestampString = columns[3].text()
+        return GroupChangesRow.parse(rowNumber, replacedString, replacementString, insertTimestampString)
     }
 
     private fun parseChangesDay(date: String, changesDivElement: Element): GroupChangesDay {
